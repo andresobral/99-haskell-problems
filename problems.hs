1ade273881e4e8f065fd4673577a2f0996a28e1c
Andre Sobral <andreqsobral@gmail.com>
2017-08-08 00:59:42 -0300

    [WIP] - Problem 03

--- problems.hs
+++ problems.hs
@@ -5,3 +5,15 @@ myLast (x:xs) = myLast(xs)
 ----- Problem 02 -----
 myButLast ([x ,xs]) = x
 myButLast (x:xs) = myButLast(xs)
+
+----- Problem 03 -----
+elementAt (x:xs) n
+  | n == 1 = x
+  | n > 1 = elementAt(xs)
+  | otherwise x
+-- elementAt [x] 1 = x
+-- elementAt (x:xs) = elementAt(xs)
+-- elementAt [x, xs] 2 = xs
+-- elementAt (x:xs) n = elementAt(xs) (n - 1)
+-- elementAt (x:xs) n
+  -- | x
