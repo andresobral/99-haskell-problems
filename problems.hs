469dcdc2f61191e2c662133ef562a88041eed679
marlon neila seron <seronmns@gmail.com>
2017-08-09 00:09:20 -0300

    Add problem descriptions

--- problems.hs
+++ problems.hs
@@ -1,25 +1,26 @@
------ Problem 01 -----
+----- Problem 01: Find the last element of a list -----
 myLast ([x]) = x
 myLast (x:xs) = myLast(xs)
 
------ Problem 02 -----
+----- Problem 02: Find the last but one element of a list -----
 myButLast ([x,xs]) = x
 myButLast (x:xs) = myButLast(xs)
 
------ Problem 03 -----
+----- Problem 03: Find the K'th element of a list. The first element in the list is number -----
+----- REDO with pattern matching ?
 elementAt (x:xs) n
   | n == 1 = x
   | n > 1 = elementAt (xs) (n - 1)
 
------ Problem 04 -----
+----- Problem 04: Find the number of elements of a list -----
 myLength ([]) = 0
 myLength ([x]) = 1
 myLength (x:xs) = 1 + myLength(xs)
 
------ Problem 05 -----
+----- Problem 05: Reverse a list -----
 myReverse ([]) = []
 myReverse ([x]) = [x]
 myReverse (x:xs) = myReverse(xs) ++ [x]
 
------ Problem 06 -----
+----- Problem 06: Find out whether a list is a palindrome -----
 isPalindrome list = list == myReverse(list)
