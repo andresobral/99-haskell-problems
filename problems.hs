36133ddfba1645ae320d09fe8487b6dcaa7f40ca
marlon neila seron <seronmns@gmail.com>
2017-08-08 23:24:14 -0300

    Problem 03: Find the K'th element of a list

--- problems.hs
+++ problems.hs
@@ -3,5 +3,10 @@ myLast ([x]) = x
 myLast (x:xs) = myLast(xs)
 
 ----- Problem 02 -----
-myButLast ([x ,xs]) = x
+myButLast ([x,xs]) = x
 myButLast (x:xs) = myButLast(xs)
+
+----- Problem 03 -----
+elementAt (x:xs) n
+  | n == 1 = x
+  | n > 1 = elementAt (xs) (n - 1)
